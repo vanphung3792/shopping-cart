1837f3d04592308379754a96b0adb2955ceaab27
Van Phung <vanphung030792@gmail.com>
2023-12-26 03:44:09 -0500

    finish

--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -1,6 +1,8 @@
 import { XMarkIcon } from "@heroicons/react/20/solid"
 import { useShoppingCart } from "../context/ShoppingCartContext"
 import CartItem from "./CartItem"
+import storeItems from "../data/items.json"
+import formatCurrency from "../utilities/formatCurrency"
 
 type  ShoppingCartProps = {
   open: boolean
@@ -41,7 +43,14 @@ return (
             <CartItem key={item.id} {...item} />
           ))}
         </div>
-        {/* CART FOOTER */}
+        <div className="font-semibold text-2xl m-5 absolute right-0 pr-5">
+          Total: {formatCurrency(
+            cartItems.reduce((total, cartItem) => {
+            const item = storeItems.find(i => i.id === cartItem.id)
+            return total + (item?.price || 0) * cartItem.quantity
+            }, 0)
+          )}
+        </div>
       </div>
     </div>
   </div>
